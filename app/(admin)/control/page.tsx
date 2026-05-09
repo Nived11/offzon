fa6c7fad39a8db1352b5b616930ee4549e3125fc
Nived <nivedsuresh5@gmail.com>
2026-05-09 14:37:36 +0530

    folder creations

---
+++ app/(admin)/control/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminDashboard() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-2xl font-bold text-red-600 bg-gray-50">
+      Admin Dashboard ( /control )
+    </div>
+  );
+}
---
+++ app/(customer)/page.tsx
@@ -0,0 +1,7 @@
+export default function CustomerHome() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-2xl text-black font-bold  bg-gray-50">
+      Customer Home Page ( / )
+    </div>
+  );
+}
